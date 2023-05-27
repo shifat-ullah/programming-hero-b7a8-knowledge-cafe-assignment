4c25ce6181dfe6f843673397fb361c22f66f80a0
Shifat ullah <shifatullah128@gmail.com>
2023-05-28 01:17:04 +0600

    telwine add

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-8xl text-yellow-600'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
